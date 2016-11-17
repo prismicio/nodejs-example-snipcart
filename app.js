a62267cdfe18fb311041f41b6dd9bbda37098cbb
Nathanael Lamelliere <nla@zenexity.com>
2016-11-17 15:48:01 +0100

    improve messages
    - localhost message
    - no Layout found message

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function render404(req, res) {
 }
 
 app.listen(PORT, function() {
-  console.log('Express server listening on port ' + PORT);
+  console.log('Point your browser to http://localhost:' + PORT);
 });
 
 app.use((req, res, next) => {
@@ -52,7 +52,7 @@ app.route('*').get((req, res, next) => {
     
     // Give an error if no layout custom type is found
     if (!layoutContent) {
-      handleError({status: 500, message: "No Layout custom type was found."}, req, res);
+      handleError({status: 500, message: "No Layout document was found."}, req, res);
     }
     
     // Define the layout content
