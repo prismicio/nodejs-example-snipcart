65824012ccfe01ee5714b1d475c423b64395b46d
Levi Gable <levimykel@gmail.com>
2017-09-28 17:48:08 +0200

    Clean up linkResolver

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  apiEndpoint: 'https://your-repo-name.prismic.io/api',
+  apiEndpoint: 'https://your-repo-name.prismic.io/api/v2',
 
   // -- Access token if the Master is not open
   // accessToken: 'xxxxxx',
@@ -14,12 +14,12 @@ module.exports = {
   // -- Links resolution rules
   // This function will be used to generate links to Prismic.io documents
   // As your project grows, you should update this function according to your routes
-  linkResolver: function (doc, ctx) {
+  linkResolver: function (doc) {
     if (doc.type == 'category') {
-      return '/category/' + encodeURIComponent(doc.uid);
+      return '/category/' + doc.uid;
     }
     if (doc.type == 'product') {
-      return '/product/' + encodeURIComponent(doc.uid);
+      return '/product/' + doc.uid;
     }
     return '/';
   }
